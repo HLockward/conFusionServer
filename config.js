0eeeaf0ceb1dd8507636b9799742658fe4eaf106
HLockward <HLockward12@gmail.com>
2019-02-06 07:33:55 -0400

    Delete config.js
    config file should not be upload

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://confusion_admin:Confusion2018@ds033699.mlab.com:33699/confusion',
-    'facebook' : {
-        'clientId' : '302449737080978',
-        'clientSecret' : '95949055f2c8b1e22ab57079c93e97d9'
-    }
-};
